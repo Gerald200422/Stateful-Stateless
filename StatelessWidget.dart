690950f340383166331ab9f4e4da5a6355c4681b
Gerald P. Balete <geraldbalete123@gmail.com>
2024-10-03 13:31:22 +0800

    Stateful-Stateless
    Stateful-Stateless

---
+++ StatelessWidget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+class Counter extends StatelessWidget {
+
+  Counter({super.key});
+
+  int _counter = 0;
+
+   void _increment(){
+    _counter++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _increment,
+          child: const Text('Increment'),
+        ),
+        const SizedBox(width: 16),
+        Text('Count: $_counter'),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
+      ),
+    ),
+  );
+}
+
+
